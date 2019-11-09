f2673b0566a2ae719639656b557d7332f5296707
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 13:31:45 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,12 @@
 class Appointment
 attr_accessor :doctor, :patient, :time
+@@all = []
 
 def initialize(time, doctor, patient)
   @time = time
   @doctor = doctor
   @patient = patient
+  @@all << self
 end
 
 end #end of class
