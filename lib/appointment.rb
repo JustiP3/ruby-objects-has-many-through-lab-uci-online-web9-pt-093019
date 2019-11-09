5323de6e3fe9e91a632e87bc4b72866ff4b5001d
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 13:30:52 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,7 +2,9 @@ class Appointment
 attr_accessor :doctor, :patient, :time
 
 def initialize(time, doctor, patient)
-
+  @time = time
+  @doctor = doctor
+  @patient = patient 
 end
 
 end #end of class
