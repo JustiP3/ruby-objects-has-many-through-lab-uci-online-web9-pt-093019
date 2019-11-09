077cdeadead13a9948e3859b0f29a92dbc87e7ae
Justin Phillips <justinphil02@yahoo.com>
2019-11-09 13:59:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -25,9 +25,9 @@ def appointments
   Appointment.all.select {|appt| appt.patient == self}
 end
 
-def doctors 
+def doctors
   appointments.map {|appt| appt.doctor}
-end 
+end
 
 
 end #end of class
